12c4c0000eecd1afb7c914a933d36d457e9c267a
bowenzhang <bowen.zhang01@outlook.com>
2023-01-08 18:22:28 +1100

    update jacksonObjectMapper

--- src/main/java/com/bowen/BZ_takeout/controller/EmployeeController.java
+++ src/main/java/com/bowen/BZ_takeout/controller/EmployeeController.java
@@ -90,6 +90,19 @@ public class EmployeeController {
 
     }
 
+    @PutMapping
+    public R<String> update (HttpServletRequest request, @RequestBody Employee employee) {
+        log.info(employee.toString());
+
+        Long empId = (Long) request.getSession().getAttribute("employee");
+        employee.setUpdateTime(LocalDateTime.now());
+        employee.setUpdateUser(empId);
+        employeeService.updateById(employee);
+
+        return R.success("successfully edited employee information.");
+    }
+
+
     @GetMapping("/{id}")
     public R<Employee> getById(@PathVariable Long id) {
         log.info("Searching employee by ID");
